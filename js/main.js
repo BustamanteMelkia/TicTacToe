3a8c7aa55d919f5aea135fab9c0a5a9e51bc7e15
Melkia <bonfil99melkia@gmail.com>
2020-10-16 17:10:49 -0500

    drawBoard function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@
 const menu = document.querySelector('#menu');
 const game = document.querySelector('#game');
 const buttonPlay = document.querySelector('#button-play');
+const GRID_SIZE = 3;
+const canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");  // get context 2D
+let maxX = canvas.width;
+let maxY = canvas.height;
+let tamC = 0;
+let px, py;
+let grid;
 
 let turn;
 let marksColor = new Map();
@@ -47,7 +55,7 @@ function onClickButtonPlay() {
         turn = playerOne
         menu.style.display = 'none';
         game.style.display = 'block';
-        renderGame(playerOne, playerTwo)
+        renderGame()
     } else
         document.querySelector('#error').innerHTML = 'Empty fields';
 }
@@ -79,13 +87,15 @@ function nextPlayerMark(currentId) {
     return currentId == 'mark-player1' ? 'mark-player2' : 'mark-player1'
 }
 
-function renderGame(playerOne, playerTwo) {
+function renderGame() {
     document.querySelector('#player1').innerHTML = playerOne.name;
     document.querySelector('#player2').innerHTML = playerTwo.name;
-    nextTurn();
+    drawBoard();
+    setTurn();
+    canvas.addEventListener('click',onClickCanvas)
 }
 
-function nextTurn(){
+function setTurn(){
     const currentMark = document.getElementById('mark');
     const active = document.getElementById('active-p'+turn.id);
     currentMark.innerHTML = turn.mark;
@@ -93,3 +103,79 @@ function nextTurn(){
     active.style.backgroundColor = turn.colorMark()
 }
 
+function getCoords(event) {
+    let x = new Number();
+    let y = new Number();
+
+    if (event.x != undefined && event.y != undefined) {
+        x = event.x;
+        y = event.y;
+    }
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+    return { x, y }
+}
+
+
+function onClickCanvas() {
+    let coords = getCoords(event);
+    px = Math.trunc(coords.x / tamC);
+    py = Math.trunc(coords.y / tamC);
+
+    if(grid[px][py] == ""){
+        grid[px][py] = turn.mark;
+        drawMark(turn.mark)
+        if(turn.id == 1)
+            turn = playerTwo
+        else
+            turn = playerOne
+        setTurn()
+    }
+}
+function drawMark(mark){
+    switch(mark){
+        case 'X': tache()
+        break
+        case 'O': circulo()
+        break;
+    }
+}
+
+function drawBoard() {
+    tamC = Math.round(maxX / GRID_SIZE);
+    for (let i = 1; i < GRID_SIZE; i++) {
+        ctx.beginPath();
+        ctx.moveTo(i * tamC, 0);
+        ctx.lineTo(i * tamC, maxY);
+        ctx.moveTo(0, i * tamC);
+        ctx.lineTo(maxX, i * tamC);
+        ctx.stroke();
+    }
+    grid = new Array(GRID_SIZE);
+    for (var i = 0; i < GRID_SIZE; i++) {
+        grid[i] = new Array(GRID_SIZE);
+        for (var j = 0; j < GRID_SIZE; j++)
+            grid[i][j] = "";
+    }
+}
+function tache() {
+    ctx.beginPath();
+
+    ctx.moveTo((px * tamC) + (tamC * .1), ((py + 1) * tamC) - (tamC * .1));         /*    /    */
+    ctx.lineTo(((px + 1) * tamC) - (tamC * .1), ((py) * tamC) + (tamC * .1));       /*   /     */
+
+    ctx.moveTo((px * tamC) + (tamC * .1), ((py) * tamC) + (tamC * .1));             /*   \    */
+    ctx.lineTo(((px + 1) * tamC) - (tamC * .1), ((py + 1) * tamC) - (tamC * .1));   /*    \    */
+
+    ctx.stroke();
+}
+
+function circulo() {
+    ctx.beginPath();
+    ctx.arc((px * tamC) + (tamC * .5), (py * tamC) + (tamC * .5), tamC * .4, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+function verifica() { }
+function reiniciar() { }
+function ganar() { }
+function perder() { }
